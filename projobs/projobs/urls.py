e19bbe49b70b9860eedf96c4e5fa93c2a1bc7e0d
Luan <luanpab@gmail.com>
2013-11-18 20:54:55 -0200

    Adding first template to project

--- projobs/projobs/urls.py
+++ projobs/projobs/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+
+from .views import HomeView
 admin.autodiscover()
 
+
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'projobs.views.home', name='home'),
+    url(r'^$', HomeView.as_view(), name='home'),
+    # Include example
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
