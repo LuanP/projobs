6cd130fa1d8ff93b4f16eecae87a40f1022bf471
Luan <luanpab@gmail.com>
2013-11-25 21:13:05 -0200

    Adding candidate homepage

--- projobs/profissionais/urls.py
+++ projobs/profissionais/urls.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 
 from django.conf.urls import patterns, include, url
+from django.contrib.auth.decorators import login_required
 
-from .views import CadastroView
+from .views import CadastroView, ProfissionalHomeView
 
 
 urlpatterns = patterns('',
+    url(r'^$', login_required(ProfissionalHomeView.as_view()), name='home'),
     url(r'^cadastro/$', CadastroView.as_view(), name='cadastro'),
 )
--- projobs/profissionais/views.py
+++ projobs/profissionais/views.py
@@ -1,9 +1,29 @@
 # -*- coding: utf-8 -*-
 
-from django.shortcuts import render
-from django.views.generic import CreateView
+from django.conf import settings
+from django.shortcuts import render, redirect
+from django.views.generic import CreateView, DetailView
 
 from .forms import ProfissionalForm
+from .models import Profissional
+
+
+class ProfissionalHomeView(DetailView):
+    model = Profissional
+    template_name = 'profissionais/home.html'
+
+    def dispatch(self, *args, **kwargs):
+        try:
+            obj = self.get_object()
+        except Profissional.DoesNotExist:
+            return redirect('{}?next={}'.format(
+                settings.LOGIN_URL,
+                self.request.path
+            ))
+        return super(ProfissionalHomeView, self).dispatch(*args, **kwargs)
+
+    def get_object(self, queryset=None):
+        return Profissional.objects.get(pk=self.request.user.pk)
 
 
 class CadastroView(CreateView):
